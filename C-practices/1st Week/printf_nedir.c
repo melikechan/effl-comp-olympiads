f140a19190e1939061a4b256b9914a8635253d43
Melike VURUCU <melikev63@gmail.com>
2021-02-16 22:55:49 +0300

    added printf's basic definition

---
+++ C-practices/1st Week/printf_nedir.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+ 	// Varsayılan girdi-çıktı
+ 	printf("hello,world");
+
+ 	// Satır atlamak için \n
+ 	printf("Merhaba\ndünya!");
+
+  	// Bir cümleyi ayırabiliriz böyle
+  	printf("Bakın böylelikle "
+  	"ayrı ayrı yazı"
+  	"yazdım.\n"
+  	);
+
+ 	// Tırnak işaretlerini yazdırmak için
+  	printf("Burası \"olimpiyat\" dersi.");
+  	return 0;
+}
