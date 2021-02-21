3c2afe81abc790ac997afb4f6936478b54c80ce0
Melike VURUCU <melikev63@gmail.com>
2021-02-21 19:49:59 +0300

    added tasks

---
+++ C-practices/1st Week/Variables/gorev3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+  int kenar1, kenar2;
+  scanf("%d %d", &kenar1, &kenar2);
+  int alan = kenar1 * kenar2;
+  printf("Alan: %d", alan);
+}
---
+++ C-practices/1st Week/Variables/gorev4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int cikolata, verilen;
+  printf("Cikolata sayisini yaziniz: ");
+  scanf("%d", &cikolata);
+  printf("Verilen sayisini yaziniz: ");
+  scanf("%d", &verilen);
+
+  if (verilen > cikolata) {
+    printf("verilen cikolata cikolata sayisindan buyuk olamaz");
+  }
+  else {
+    printf("%d", cikolata - verilen);
+  }
+}
