1f5491df6e0611747b90a34881214d8ac5661647
Melike Vurucu <melikechan@tuta.io>
2021-02-21 21:20:12 +0300

    changed file paths

---
+++ C-practices/1st-week/ifelse.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+
+int main(void) {
+  	int girilen;
+  	scanf("%d", &girilen);
+
+  	// Eğer girilen sayı 21'e eşit değil ise
+  	if (girilen != 21) {
+    	printf("21'e esit degildir.\n");
+	}
+
+  	// Eğer girilen sayı 21'e eşit ise
+	if (girilen == 21) {
+		printf("21'e esittir.\n");
+	}
+
+
+	// Eğer girilen sayı 20'den küçük ise
+	if (girilen < 20) {
+		printf("20'den kucuk.\n");
+	}
+	// Eğer girilen sayı 20'ye büyük eşit ve 50'den küçük ise
+	else if (girilen >= 20 && girilen < 50) {
+		printf("20'den buyuk, 50'denk kucuk");
+	}
+	else {
+		printf("50'den buyuk.")
+	}
+ 	return 0;
+}
