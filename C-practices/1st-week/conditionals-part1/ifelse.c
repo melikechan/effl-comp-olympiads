e6d7fc6385d1ca462dc9b272180ecbe86b0ac7a8
Melike VURUCU <melikev63@gmail.com>
2021-02-26 08:38:24 +0300

    seperated logic operations

--- C-practices/1st-week/conditionals-part1/ifelse.c
+++ C-practices/1st-week/conditionals-part1/ifelse.c
@@ -1,33 +1,36 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-int main(void) {
-  	int girilen;
-  	scanf("%d", &girilen);
+int main(void)
+{
+    int girilen;
+    scanf("%d", &girilen);
 
-  	// Eğer girilen sayı 21'e eşit değil ise
-  	if (girilen != 21) {
-    	printf("21'e esit degildir.\n");
-	}
+    // Eğer girilen sayı 21'e eşit değil ise
+    if (girilen != 21)
+    {
+        printf("21'e esit degildir.\n");
+    }
 
-  	// Eğer girilen sayı 21'e eşit ise
-	if (girilen == 21) {
-		printf("21'e esittir.\n");
-	}
+    // Eğer girilen sayı 21'e eşit ise
+    if (girilen == 21)
+    {
+        printf("21'e esittir.\n");
+    }
 
-
-	// Eğer girilen sayı 20'den küçük ise
-	if (girilen < 20) {
-		printf("20'den kucuk.\n");
-	}
-	// Eğer girilen sayı 20'ye büyük eşit ve 50'den küçük ise
-	else if (girilen >= 20 && girilen < 50) {
-		printf("20'den buyuk, 50'denk kucuk");
-	}
-	// Eğer girilen sayı 50'den büyük ise
-	else {
-		printf("50'den buyuk.");
-	}
-
-	// && Ve || veya demek (mantıksal operatörler)
- 	return 0;
+    // Eğer girilen sayı 20'den küçük ise
+    if (girilen < 20)
+    {
+        printf("20'den kucuk.\n");
+    }
+    // Eğer girilen sayı 20'ye büyük eşit ve 50'den küçük ise
+    else if (girilen >= 20 && girilen < 50)
+    {
+        printf("20'den buyuk, 50'denk kucuk");
+    }
+    // Eğer girilen sayı 50'den büyük ise
+    else
+    {
+        printf("50'den buyuk.");
+    }
+    return 0;
 }
