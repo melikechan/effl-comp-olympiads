9567d5a34bd9353c34d546c8bdfe94136a8f817b
Melike VURUCU <melikev63@gmail.com>
2021-02-26 08:37:27 +0300

    added primitive sizes

--- C-practices/1st-week/Variables/degiskenler.c
+++ C-practices/1st-week/Variables/degiskenler.c
@@ -1,28 +1,55 @@
 #include <stdio.h>
+#include <limits.h>
+#include <float.h>
 
-int main(void) {
-    // 1. Elma armut vb. alın marketten. Onların fiyatını verin ve toplam fiyatı yazdırın.
-    // 2. Çikolatamız olsun. onları ondalıklı sayı şeklinde bölelim. 
-    // İki değişken olsun, çikolata ve kişi sayısı. 
-    // Onları birbirine bölüp bir değişkene atayıp (double) yazdırın.
-
-    // 3. görevimiz: dikdörtgenin alanını bulan bir program yazın
-    // 4. görevimiz: Çikolata sayısını ve arkadaşlara kaç çikolata verdiğiniz iki değişken olsun.
-    // Elinizde kaç çikolara kaldığını yazdırın 
-
-    /* değişkenler, tam sayı, ondalıklı sayı, dizi, string, karakter ve benzeri olabilir.
-        Örneğin bu şekilde tanımlayabilirim.
+int main(void)
+{
+    /* Değişken dediğimiz şey, birden fazla kez kullanacağımız değerleri tutmaya yarar.
+       Bu değerler tam sayı, harf, ondalıklı sayı, dizi vb. olabilir.
     */
-
+    // Örneğin, tanımlamak için şöyle yaparız:
+    // değer türü, değişken adı, (varsa) değeri
     int daireYaricapi = 5;
-    // integer 4 byte, short 2 byte, char 1 byte, double 8 byte şeklinde gitmektedir.
     float daireYaricapi2;
     scanf("%f", &daireYaricapi2);
-    // const, degismeyen degiskenler
 
-    const double PI = 3.14;
-    
-    printf("Kullanıcı tanımlı dairenin alanı: %lf:", daireYaricapi2 * daireYaricapi2 * PI);
-    printf("Dairenin alanı: %lf \n", daireYaricapi * daireYaricapi * PI);
-    return 0;
+    // Her değişkenin kapladığı bir yer vardır.
+    /*Unsigned değişkenler, pozitiftir (fakat 0 dahil). Eğer sonuç negatif ise o değerin maksimum değeri + 1 + sayi (negatif olur)
+      Maksimum değerler ise 2^(kapladığı alan (bit)) - 1 şeklinde alınabilir
+      (-1 olmasının nedeni sıfırı da dahil etmemizdir.)
+      Örneğin char değişkeni 8 bit yer kaplar. Yani, unsigned olarak maksimum 2^8 - 1 (255) alabilir.
+     */
+    /* Signed yani işareti bulunan değişkenlerde maksimum değer 2^(kapladığı alan (bit) - 1) - 1 olabilir.
+       Örneğin char değişkeni 8 bit yer kaplar. Yani, signed olarak maksimum 2^7 - 1 (127) alabilir.
+       (-1 olmasının nedeni sıfırı da dahil etmemizdir.)
+       Minimum değer ise -2^(kapladığı alan (bit) - 1) olabilir.
+      Örneğin char değişkeni 8 bit yer kaplar. Yani, signed olarak minimum -2^7 (-128) alabilir.
+    */
+    printf("\nDegisken turu:\t Boyut:\t\t Minimum deger:\t\t Maximum deger:\t\t Aciklama:\t\t\n");
+    printf("--------------------------------------------------------------------------------------------\n");
+    printf("char\t\t%d\t\t%d\t\t\t%d\t\t\tKarakterleri ifade etmek icin kullanilir.\n", 
+    sizeof(char), CHAR_MIN, CHAR_MAX);
+    printf("unsigned char\t%d\t\t%d\t\t\t%d\t\t\tKarakterleri ifade etmek icin kullanilir."
+    "\n\t\t\t\t\t\t\t\t\t\tFakat dogal sayidir.\n",
+    sizeof(unsigned char), 0, UCHAR_MAX);
+    printf("short\t\t%d\t\t%d\t\t\t%d\t\t\tKucuk tam sayilari ifade etmek icin kullanilir.\n",
+    sizeof(short), SHRT_MIN, SHRT_MAX);
+    printf("unsigned short  %d\t\t%d\t\t\t%d\t\t\tKucuk dogal sayilari ifade etmek icin kullanilir.\n",
+    sizeof(unsigned short), 0, USHRT_MAX);
+    printf("int\t\t%d\t\t%d\t\t%d\t\tTam sayilari ifade etmek icin kullanilir.\n",
+    sizeof(int), INT_MIN, INT_MAX);
+    printf("unsigned int\t%d\t\t%d\t\t\t%lld\t\tDogal sayilari ifade etmek icin kullanilir.\n",
+    sizeof(unsigned int), 0, UINT_MAX);
+    printf("long\t\t%d\t\t%d\t\t%ld\t\t32 bit'e cikabilen tam sayilar icin kullanilir.\n",
+    sizeof(long), LONG_MIN, LONG_MAX);
+    printf("unsigned long\t%d\t\t%d\t\t\t%lld\t\t32 bit'e cikabilen dogal sayilar icin kullanilir.\n",
+    sizeof(unsigned long), 0, ULONG_MAX);
+    printf("long long\t%d\t\t%lld\t%lld\tBuyuk tam sayilari ifade etmek icin kullanilir.\n",
+    sizeof(long long), LLONG_MIN, LLONG_MAX);
+    printf("unsigned ll\t%d\t\t%d\t\t\t%llu\tBuyuk dogal sayilari ifade etmek icin kullanilir.\n",
+    sizeof(unsigned long long), 0, ULLONG_MAX);
+    printf("float\t\t%d\t\t%.0f\t\t\t%.0f\t\t\nOndalikli sayilari ifade etmek icin kullanilir.\n",
+    sizeof(float), FLT_MIN, FLT_MAX);
+    printf("double\t\t%d\t\t%.lf\t\t\t%.0lf\t\t\nDaha kesin ondalikli sayilari ifade etmek icin kullanilir.\n",
+    sizeof(int), DBL_MIN, DBL_MAX);
 }
