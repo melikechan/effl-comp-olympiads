7d8e0dc67e598411c432db4f7d3d6cc17fc3ade9
Melike Vurucu <melikechan@tuta.io>
2021-03-01 11:02:17 +0300

    added missions and answers

---
+++ C-practices/1st-week/conditionals-part1/missions/gorev1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int sayi;
+    scanf("%d", &sayi);
+
+    if (sayi >= 100 && sayi <= 140) {
+        printf("Sayi 100 ile 140 arasindadir.\n");
+    }
+    else {
+        printf("Sayi bu aralik icerisine dahil degildir.\n");
+    }
+    return 0;
+}
