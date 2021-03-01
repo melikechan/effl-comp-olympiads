08594e44cd1d7210789e469964d108ee0dac7ef5
Melike Vurucu <melikechan@tuta.io>
2021-03-01 11:34:19 +0300

    added explainations to some operations

---
+++ C-practices/1st-week/Variables/aritmetik_operatorler.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+  /*
+  İşlemler:
+    + -> toplama operatörü
+    - -> çıkarma operatörü 
+    * -> çarpma operatörü
+    /  -> bölme operatörü
+    % -> modül operatörü (bir sayıyla bölümünden kalanı buldurmaya yarar)
+  Atama:
+    = -> bir sayıyı bir değişkene atamaya yarar
+    (+-/*%)= -> sağdaki değeri soldaki değer ile işleme sokar
+      yani, değişkenimiz d, işlem yapılan da i olmak üzere:
+      d += i -> d = d + i
+      d -= i -> d = d - i
+      d *= i -> d = d * i
+      d /= i -> d = d / i
+      d %= i -> d = d % i
+  */
+  return 0;
+}
