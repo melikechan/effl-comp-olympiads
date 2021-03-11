2f216fa53e100f36cc81c71d0bd5024c8c14c1e1
Melike Vurucu <melikechan@tuta.io>
2021-03-11 18:53:30 +0300

    added switch-case code

--- C-practices/2nd-week/conditionals-part2/switch-case.c
+++ C-practices/2nd-week/conditionals-part2/switch-case.c
@@ -7,26 +7,119 @@ int main(void)
         bu sabit durumlar harici durumlarda da default kullanabiliriz.
         */
 
-       /*
-       ** DİKKAT EDİLMESİ GEREKENLER: **
-		* Case yani durumlar sabit bir değer olmalı. Bir değişken (sabit olmayan) koyarsak hata verir.
-        Ve bu sabit değerimiz enum (kullanıcı tanımlı değişken), integer ya da char olmalıdır.
-		* Case'lerden sonra eğer birden fazla değerin aynı işlemi yapmasını 
-		istiyorsak break koymayabiliriz fakat eğer yalnızca bir değer için o işlemi yaptıracaksak break koymalıyız.
-		örneğin: 
-		case 8:
-			kodumuz
-			break; (eğer değerimiz 8 ise yalnızca case 8'i çalıştıracaktır)
-        case 9:
-            kodumuz (eğer değerimiz 9 ise case 9'da break olmadığı için 
-            case 10'u da çalıştıracaktır case 9 ile)
-        case 10:
-            kodumuz
-            break;
+    int i = 8;
 
-        * iki case bloğu aynı değere sahip olamaz.
-        * case bloğu'nun altındaki case altında olmayan satırlar çalıştırılmaz.
-        */
-     
+    switch (i)
+    {
+    case 4:
+        printf("4\n");
+        break;
+    case 5:
+        printf("5\n");
+        break;
+    case 7:
+        printf("7\n");
+        break;
+    case 8:
+        printf("8\n");
+        break;
+    default:
+        printf("disinda\n");
+        break;
+    }
+
+    /*
+       ** DİKKAT EDİLMESİ GEREKENLER: **
+    */
+
+    /* Case'lerden sonra eğer birden fazla değerin aynı işlemi yapmasını 
+		istiyorsak break koymayabiliriz fakat eğer yalnızca bir değer için 
+        o işlemi yaptıracaksak break koymalıyız.
+		örneğin: */
+    switch (i)
+    {
+    case 4:
+        printf("4\n");
+        break;
+    case 5:
+        printf("5\n");
+        break;
+    case 7:
+        printf("7\n"); /* break olmadığı için 
+            case 7'yi de çalıştıracaktır case 8 ile*/
+    case 8:
+        printf("8\n");
+        break;
+    default:
+        printf("disinda\n");
+        break;
+    }
+
+    // case bloğu'nun altındaki case altında olmayan satırlar çalıştırılmaz.
+    switch (i)
+    {
+        i += 1;
+    case 4:
+        printf("4\n");
+        break;
+    case 5:
+        printf("5\n");
+        break;
+    case 7:
+        printf("7\n");
+        break;
+    case 8:
+        printf("8\n");
+        break;
+    case 9:
+        printf("9\n");
+        break;
+    default:
+        printf("disinda\n");
+        break;
+    }
+
+    // iki case bloğu aynı değere sahip olamaz.
+    /*switch (i)
+    {
+    case 8:
+        printf("8\n");
+        break;
+    case 8:
+        printf("baska durum\n");
+        break;
+    default:
+        break;
+    }*/
+
+    // Case yani durumlar sabit bir değer olmalı. Bir değişken (sabit olmayan) koyarsak hata verir.
+
+    /*switch (i)
+    {
+    case i + 1:
+        printf("%d\n", i + 1);
+        break;
+    case 10:
+        printf("10\n");
+        break;
+    default:
+        printf("idk");
+        break;
+    }*/
+
+    // Ve bu sabit değerimiz enum (kullanıcı tanımlı değişken), integer ya da char olmalıdır.
+    /*switch (i)
+    {
+    case 1.1:
+        printf("%d\n");
+        break;
+    case 10:
+        printf("10\n");
+        break;
+    default:
+        printf("idk");
+        break;
+    }*/
+
     return 0;
 }
