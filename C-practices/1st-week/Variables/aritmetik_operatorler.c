f31bfec157190d0a307b4c0ae6d678b3c8d6883a
haxala1r <aemin375@gmail.com>
2021-03-12 20:59:41 +0300

    Added some comments, refactored some of the code

--- C-practices/1st-week/Variables/aritmetik_operatorler.c
+++ C-practices/1st-week/Variables/aritmetik_operatorler.c
@@ -4,41 +4,47 @@ int main(void)
 {
     int a = 45, b = 20;
     /*
-  İşlemler:
-    + -> toplama operatörü
-    - -> çıkarma operatörü 
-    * -> çarpma operatörü
-    /  -> bölme operatörü
-    % -> modül operatörü (bir sayıyla bölümünden kalanı buldurmaya yarar)
-  Atama:
-    = -> bir sayıyı bir değişkene atamaya yarar
-    (+-/*%)= -> sağdaki değeri soldaki değer ile işleme sokar
-      yani, değişkenimiz d, işlem yapılan da i olmak üzere:
-      d += i -> d = d + i
-      d -= i -> d = d - i
-      d *= i -> d = d * i
-      d /= i -> d = d / i
-      d %= i -> d = d % i
-  */
-    // Aritmetik operatörler
+       İşlemler:
+          + -> toplama operatörü
+          - -> çıkarma operatörü 
+          * -> çarpma operatörü
+          / -> bölme operatörü
+          % -> modül operatörü (bir sayıyla bölümünden kalanı buldurmaya yarar)
+       Atama:
+          =       -> bir sayıyı bir değişkene atamaya yarar
+         (+-/*%)= -> sağdaki değeri soldaki değer ile işleme sokar
+                     yani, değişkenimiz d, işlem yapılan da i olmak üzere:
+           d += i -> d = d + i
+           d -= i -> d = d - i
+           d *= i -> d = d * i
+           d /= i -> d = d / i
+           d %= i -> d = d % i
+    */
     printf("%d\n", a + b);
     printf("%d\n", a - b);
     printf("%d\n", a * b);
     printf("%d\n", a / b);
     printf("%d\n", a % b);
-
+    
+    // a ile b'yi toplayıp sonucu a'ya atar.
     a += b;
     printf("%d\n", a);
-
+    
     a = 45;
+    
+    // a'dan b'yi çıkarıp sonucu a'ya atar.
     a -= b;
     printf("%d\n", a);
 
     a = 45;
+    
+    // a ile b'yi çarpıp sonucu a'ya atar.
     a *= b;
     printf("%d\n", a);
 
     a = 45;
+    
+    // a'yı b'ye bölüp sonucu a'ya atar.
     a /= b;
     printf("%d\n", a);
 
--- C-practices/1st-week/Variables/missions/gorev4.c
+++ C-practices/1st-week/Variables/missions/gorev4.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-  int cikolata, verilen;
-  printf("Cikolata sayisini yaziniz: ");
-  scanf("%d", &cikolata);
-  printf("Verilen sayisini yaziniz: ");
-  scanf("%d", &verilen);
+    int cikolata, verilen;
+    
+    printf("Cikolata sayisini yaziniz: ");
+    scanf("%d", &cikolata);
+    
+    printf("Verilen sayisini yaziniz: ");
+    scanf("%d", &verilen);
 
-  if (verilen > cikolata) {
-    printf("verilen cikolata cikolata sayisindan buyuk olamaz");
-  }
-  else {
-    printf("%d", cikolata - verilen);
-  }
+    if (verilen > cikolata) {
+      printf("verilen cikolata cikolata sayisindan buyuk olamaz");
+    }
+    else {
+      printf("%d", cikolata - verilen);
+    }
 }
