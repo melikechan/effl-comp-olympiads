3a20cad0f958f7fbe0f6fcf76e6f6408d8631d5e
haxala1r <aemin375@gmail.com>
2021-03-13 14:59:43 +0300

    minor reformatting

--- C-practices/1st-week/Variables/degiskenler.c
+++ C-practices/1st-week/Variables/degiskenler.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	/*
 	Değişken dediğimiz şey, birden fazla kez kullanacağımız değerleri tutmaya yarar.
-    Bu değerler tam sayı, harf, ondalıklı sayı, dizi vb. olabilir.
+	Bu değerler tam sayı, harf, ondalıklı sayı, dizi vb. olabilir.
 
 	Örnek bir değişken tanımı şöyledir:
 		(Değişken tipi) (değişken adı) ([Eğer en başta değer atanacaksa] = deger) ;
@@ -14,29 +14,31 @@ int main(void)
 		daire_yaricapi  -> değişkenin adı. Bu değişkeni tekrar kullanırken
 							bu ismi kullanırız.
 		5               ->	değişkenin değeri. Burada 5 değeri atanmış.
-        ;               ->	Bu "ifade"nin sona erdiğini ifade eder. C
+		;               ->	Bu "ifade"nin sona erdiğini ifade eder. C
 							programlama dilinde her ifade (burada değişken
 							tanımladık) noktalı virgül ile sona ermelidir.
-    */
+	*/
 	int daire_yaricapi = 5;
 
-	/*  Bu ikinci örnekte ise değişkene bir değer atanmamış.
+	/*
+		Bu ikinci örnekte ise değişkene bir değer atanmamış.
 		İnceleyelim:
 			float           -> değişkenin tipi. "float", yani "floating point number"
-                            ondalıklı sayı anlamındadır.
+								ondalıklı sayı anlamındadır.
 			daire_yaricapi2 -> değişkenin adı.
 			;              -> ifade bitirilmiş.
 
 		Bir değişkeni tanımlarken ona bir değer atamak zorunluluğunuz yoktur.
 		Burada da olduğu gibi değişkeni önce tanımlayıp değerini sonradan
 		atayabilirsiniz.
-    */
+	*/
 	float daire_yaricapi2;
 
-	/*  Bir değişkene değer atama işlemi böyle yapılır.
+	/*
+		Bir değişkene değer atama işlemi böyle yapılır.
 		Burada "daireYaricapi" değişkeninin değeri "daireYaricapi2"
 		değişkenine aktarılmış.
-    */
+	*/
 	daire_yaricapi2 = daire_yaricapi;
 
 	/* Bu ifade "daire_yaricapi2" değişkeninin değerini ekrana yazar. */
