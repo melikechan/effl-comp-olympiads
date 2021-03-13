54cf60d1048537d2ea7a83f27918a3f2ce2c300a
haxala1r <aemin375@gmail.com>
2021-03-13 14:49:31 +0300

    minor fixes

--- C-practices/1st-week/Variables/aritmetik_operatorler.c
+++ C-practices/1st-week/Variables/aritmetik_operatorler.c
@@ -4,22 +4,22 @@ int main(void)
 {
 	int a = 45, b = 20;
 	/*
-       İşlemler:
-          + -> toplama operatörü
-          - -> çıkarma operatörü
-          * -> çarpma operatörü
-          / -> bölme operatörü
-          % -> modül operatörü (bir sayıyla bölümünden kalanı buldurmaya yarar)
-       Atama:
-          =       -> bir sayıyı bir değişkene atamaya yarar
-         (+-/*%)= -> sağdaki değeri soldaki değer ile işleme sokar
-                     yani, değişkenimiz d, işlem yapılan da i olmak üzere:
-           d += i -> d = d + i
-           d -= i -> d = d - i
-           d *= i -> d = d * i
-           d /= i -> d = d / i
-           d %= i -> d = d % i
-    */
+		İşlemler:
+			+ -> toplama operatörü
+			- -> çıkarma operatörü
+			* -> çarpma operatörü
+			/ -> bölme operatörü
+			% -> modül operatörü (bir sayıyla bölümünden kalanı buldurmaya yarar)
+		Atama:
+			=        -> bir sayıyı bir değişkene atamaya yarar
+			(+-/*%)= -> sağdaki değeri soldaki değer ile işleme sokar
+						yani, değişkenimiz d, işlem yapılan da i olmak üzere:
+			d += i -> d = d + i
+			d -= i -> d = d - i
+			d *= i -> d = d * i
+			d /= i -> d = d / i
+			d %= i -> d = d % i
+	*/
 	printf("%d\n", a + b);
 	printf("%d\n", a - b);
 	printf("%d\n", a * b);
