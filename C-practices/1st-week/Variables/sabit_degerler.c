68bfefd0dbada852d8dc1da6411a62c9ee2601e5
haxala1r <aemin375@gmail.com>
2021-03-13 14:41:03 +0300

    Changed some files to follow the new formatting rules

--- C-practices/1st-week/Variables/sabit_degerler.c
+++ C-practices/1st-week/Variables/sabit_degerler.c
@@ -2,17 +2,19 @@
 
 int main(void)
 {
-    float daireYaricapi;
-    scanf("%f", &daireYaricapi);
+    float daire_yaricapi;
+    scanf("%f", &daire_yaricapi);
 
-    /* Bir değişkeni anımlarken tipinden önce "const" yani "constant" 
-       getirilirse, o değişken bir daha değiştirilemez.
-       "const" ifadesinin dışında değişken tanımında herhangi bir değişiklik
-       yoktur.
+    /* Bir değişkeni anımlarken tipinden önce "const" yani "constant"
+	   getirilirse, o değişken bir daha değiştirilemez.
+	   "const" ifadesinin dışında değişken tanımında herhangi bir değişiklik
+	   yoktur.
+	   Bu şekilde tanımlanan değişkenler genellikle tamamen büyük harflerle
+	   adlandırılır, ancak bu bir zorunluluk değildir.
     */
     const double PI = 3.14;
 
-    printf("Dairenin alanı: %lf \n", daireYaricapi * daireYaricapi * PI);
+    printf("Dairenin alanı: %lf \n", daire_yaricapi * daire_yaricapi * PI);
 
     return 0;
 }
