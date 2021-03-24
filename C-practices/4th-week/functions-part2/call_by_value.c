e3842ed8c53a70b0c6fb9bedf0832ac63f7f0267
Melike VURUCU <melikev63@gmail.com>
2021-03-24 09:53:15 +0300

    added call by value

---
+++ C-practices/4th-week/functions-part2/call_by_value.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void degistir_ptr(int *num)
+{
+	*num = 20;
+}
+
+void degistir(int num)
+{
+	num = 20;
+}
+int main(void)
+{
+	int a = 50;
+	int b = 60;
+	printf("A: %d\n", a);
+	degistir_ptr(&a);
+	printf("A'yı arttiriyoruz: %d\n", a);
+
+	printf("B: %d\n", b);
+	degistir(b);
+	printf("B'yi arttirmayi deniyoruz: %d\n", b);
+	return 0;
+}
