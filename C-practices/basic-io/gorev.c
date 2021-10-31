a0962b824b5e461cc3b330f5f9cc3cf823fec147
Melike Vurucu <melikechan@tuta.io>
2021-10-31 20:24:49 +0300

    added solution of the question

---
+++ C-practices/basic-io/gorev.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (void) {
+	printf("Merhaba dunya\tben \"C\" programi yazabiliyorum.\n");
+	printf("\\ters egik cizgi\\ nasil yazdirabilirim?\n");
+	printf("Bir satir\nbile \"bana\"\nyetmiyor");
+	return 0;
+}
